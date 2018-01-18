974b7b6c2ded12d92bc33f56685ab9ea0a6b19b5
kamil <xdreamer93@o2.pl>
2018-01-18 20:12:43 +0100

    Added simple upload file form

--- TsiiApp/Controllers/HomeController.cs
+++ TsiiApp/Controllers/HomeController.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Web.Mvc;
-using System.Linq;
-using TsiiApp.ViewModels;
+using System.Web;
 using TsiiApp.Services;
 
 namespace TsiiApp.Controllers
@@ -11,12 +10,10 @@ namespace TsiiApp.Controllers
 	{
 		public ActionResult Index()
 		{
-			
 			var service = new ChartService();
 			var homeViewModel = service.GetChartsFileNames();
-			
+
 			return View(homeViewModel);
-			
 		}
 
 		[HttpGet]
@@ -25,5 +22,28 @@ namespace TsiiApp.Controllers
 			return View();
 		}
 
+		[HttpPost]
+		public ActionResult AddNewFile(HttpPostedFileBase file)
+		{
+			if (file != null && file.ContentLength > 0)
+			{
+				try
+				{
+					string path = Path.Combine(Server.MapPath("~/Charts"), Path.GetFileName(file.FileName));
+					file.SaveAs(path);
+					ViewBag.Message = "File uploaded successfully";
+				}
+				catch (Exception ex)
+				{
+					ViewBag.Message = "ERROR:" + ex.Message;
+				}
+			}
+			else
+			{
+				ViewBag.Message = "You have not specified a file.";
+			}
+
+			return View();
+		}
 	}
 }
